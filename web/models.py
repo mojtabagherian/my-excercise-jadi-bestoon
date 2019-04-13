ee3343636b913b7f5241a1e6f6246eaf7cf7e726
mojtabarmm <mojtabarmm@gmail.com>
2019-04-13 22:11:01 +0430

    submit/expense and submit/income

--- web/models.py
+++ web/models.py
@@ -4,6 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __unicode__(self):
+        return "{} token".format(self.user)
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -18,4 +23,3 @@ class Income(models.Model):
     user = models.ForeignKey(User)
     def __unicode__(self):
         return "{}----{}----{}".format(self.text, self.amount, self.date)
- 
--- web/views.py
+++ web/views.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from json import JSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+from web.models import User, Token, Expense, Income
+from datetime import datetime
 # Create your views here.
+@csrf_exempt
+def submit_income(request):
+    """user submit an income"""
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Income.objects.create(user = this_user, amount=request.POST['amount'],
+             text = request.POST['text'], date = date)
+
+
+@csrf_exempt
+def submit_expense(request):
+    """user submit an expense"""
+    this_token = request.POST['token']
+    this_user = User.objects.filter(token__token = this_token).get()
+    if 'date' not in request.POST:
+        date = datetime.now()
+    Expense.objects.create(user = this_user, amount=request.POST['amount'],
+             text = request.POST['text'], date = date)
+
+
+    return JsonResponse({
+    'status':'ok',
+    },encoder=JSONEncoder)
